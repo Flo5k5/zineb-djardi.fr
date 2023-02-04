791cc070238a50a6d3e92263fb81a55d62492bbf
Zineb <zineb.djardi@gmail.com>
2023-02-04 17:15:27 +0100

    slider-2

--- src/globals.js
+++ src/globals.js
@@ -17,3 +17,28 @@ global.RSP_MD = 992;
 global.RSP_LG = 1200;
 global.RSP_XL = 1400;
 global.RSP_XXL = 1600;
+
+
+////////////////////////////////////////////////////////////////
+// SLIDER METHODE
+////////////////////////////////////////////////////////////////
+
+
+    // Select the slide container and menu list items
+const slideContainer = document.querySelector('.slide_container');
+const menuItems = document.querySelectorAll('.menu li');
+
+// Loop through the menu items and add a click event to each one
+menuItems.forEach((menuItem, index) => {
+  menuItem.addEventListener('click', () => {
+    // Remove the active class from all menu items
+    menuItems.forEach(item => item.classList.remove('active'));
+    // Add the active class to the clicked menu item
+    menuItem.classList.add('active');
+    // Update the slideContainer to show the corresponding slide
+    slideContainer.style.transform = `translateX(-${index * 100}%)`;
+  });
+});
+
+  
+  
