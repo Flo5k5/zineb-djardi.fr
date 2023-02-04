c32618c23118f1c459070e30b5d7d41d688cd0e5
Zineb <zineb.djardi@gmail.com>
2023-02-04 09:31:38 +0100

    slider

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
 import GLOBALS from "./globals";
 import * as responsive from "./utils/responsive";
-import Navigation from "./classes/Navigation";
+//import Navigation from "./classes/Navigation";
 import Scroll from "./classes/Scroll";
-import Animation from "./classes/Animation";
-import Transition from "./classes/Transition";
-import RAF from "./classes/RAF";
-import Videoblock from "./classes/Videoblock";
-import Loader from "./classes/Loader";
+//import Animation from "./classes/Animation";
+//import Transition from "./classes/Transition";
+//import RAF from "./classes/RAF";
+//import Videoblock from "./classes/Videoblock";
+//import Loader from "./classes/Loader";
 
 responsive.cssSetTouchDevices("html", "touch", "no_touch");
 
-export const navigation = new Navigation();
+//export const navigation = new Navigation();
 export const scroll = new Scroll();
-export const transition = new Transition();
-export const animation = new Animation();
-const raf = new RAF();
-export const videoblock = new Videoblock();
-const loader = new Loader();
+//export const transition = new Transition();
+//export const animation = new Animation();
+//const raf = new RAF();
+//export const videoblock = new Videoblock();
+//const loader = new Loader();
 
-const header = document.querySelector("#header");
+
 
 window.addEventListener("load", () => {
-  loader.hideLoader();
+ // loader.hideLoader();
   scroll.init();
-  scroll.action();
-  raf.render();
+ // scroll.action();
+ // raf.render();
 });
+
+const header = document.querySelector("#header");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 200) {
